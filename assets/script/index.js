48c38709b2e748640c714bca37e4de183a901be4
tamanchichan <tamanchichan@gmail.com>
2023-03-11 15:20:28 -0600

    sound added

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,11 +3,19 @@
 const output = document.querySelector('.output');
 const generateButton = document.querySelector('.button-generate');
 
+const soundEffect = new Audio('./assets/audio/sound-effect.wav');
+soundEffect.type = 'audio/wav';
+
 // Return a random number between 0 and 50.
 function generateNumber() {
   return Math.floor(Math.random() * 50);
 }
 
+function playSoundEffect() {
+  soundEffect.play();
+}
+
 generateButton.addEventListener('click', () => {
   output.innerText = generateNumber();
+  playSoundEffect();
 });
