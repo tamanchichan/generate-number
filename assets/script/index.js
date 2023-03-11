ee1fd619fad2664e21be91213fd38135802af863
tamanchichan <tamanchichan@gmail.com>
2023-03-11 15:09:03 -0600

    layout styled

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,13 @@
 'use strict';
 
+const output = document.querySelector('.output');
+const generateButton = document.querySelector('.button-generate');
+
 function generateNumber() {
   return Math.floor(Math.random() * 50);
   // return a random number between 0 and 50
 }
+
+generateButton.addEventListener('click', () => {
+  output.innerText = generateNumber();
+});
